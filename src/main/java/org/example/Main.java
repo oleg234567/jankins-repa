1e8823c2d818fd9025791aa7fe9393433d0ca3d1
oleg234567 <okim45033@gmail.com>
2024-08-26 21:31:06 +0900

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+public static final String oleg = "oleg";
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = a + b;
+        String surname = "oleg";
+        System.out.print(c);
+        System.out.println(surname);
+    }
+
+}
